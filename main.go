d6fa009f9d5e3b0efbf761c19beaac36d7f29cf7
Vital1992 <vital09.92@gmail.com>
2023-10-16 12:20:47 -0700

    Fix port

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	http.Handle("/api", router)
 
-	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
 
 	bot, err := chatbot.NewBotAPI(os.Getenv("API_KEY"))
 	if err != nil {
